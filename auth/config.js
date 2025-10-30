6ab141f152efef18d637f18db645389ab80feb89
stevens-cofel <commercial@deco72.com>
2025-10-30 09:20:37 +0100

    Update config.js

--- auth/config.js
+++ auth/config.js
@@ -1,7 +1,7 @@
-// /auth/config.js  (AUCUNE balise <script> ici)
+// /auth/config.js  (JS pur, aucune balise <script>, aucun commentaire HTML)
 export const AUTH_SETTINGS = {
   siteId: "cofel-configs",
-  // SHA-256 de "Cofel2025!"
+  // Empreinte SHA-256 de "Cofel2025!"
   passwordHash: "f754854329ca38b7e07239bc592c685c8bfe9300c9b5c5b587798e035f7d48cd",
   tokenDays: 7
 };
